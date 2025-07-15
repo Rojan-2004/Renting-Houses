57be18f6a70feb11d03964b607ea6a6a248a937a
Rojan-2004 <rojanm1000@gmail.com>
2025-07-15 14:47:04 +0545

    home push

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './AdminDashboard.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
